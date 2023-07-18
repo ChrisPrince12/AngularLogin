4ff3ad3a1a4522698c304996f7a375e7ecdecd81
Christopher John <chrispluts@gmail.com>
2023-07-18 12:26:52 +0300

    Addition Of Profile Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './service/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  public isLogin:boolean=false;
+
   title = 'loginRegister';
+
+  constructor(private authService: AuthenticationService){}
+
+  ngOnInit(): void {
+    this.authService.isAuthenticated$.subscribe(
+      isAuthenticated => {
+        this.isLogin = isAuthenticated;
+      }
+    )
+  }
 }
